157a7c1e43ca3215e193bfbf01ccb5e4e1b1154a
Dipokal HHJ <hhj@devent.kr>
2023-05-06 12:43:10 +0900

    feat: get test data

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy
 import os
 import tensorflow as tf
 
 print(tf.version.VERSION)
+IMAGENET_TEST_IMAGE = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+UNZIP_PATH = tf.keras.utils.get_file("cats_and_dogs.zip", origin=IMAGENET_TEST_IMAGE, extract=True)
+PATH = os.path.join(os.path.dirname(UNZIP_PATH), 'cats_and_dogs_filtered')
+
+train_dir = os.path.join(PATH, "train")
+validation_dir = os.path.join(PATH, "validation")
+
+BATCH_SIZE = 32
+IMAGE_SIZE = (160, 160)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    train_dir, 
+    shuffle=True, 
+    batch_size=BATCH_SIZE,
+    image_size=IMAGE_SIZE
+)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    validation_dir,
+    shuffle=True,
+    batch_size=BATCH_SIZE,
+    image_size=IMAGE_SIZE
+)
+
+class_names = train_dataset.class_names
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+plt.show()
+
 
 model = tf.keras.models.Sequential([
     tf.keras.applications.MobileNetV3Small(
