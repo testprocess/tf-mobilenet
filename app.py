2e08d665c368d02f885fe007ccaa77dc2899811d
Dipokal HHJ <hhj@devent.kr>
2023-05-23 21:11:09 +0900

    feat: add train code

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 import tensorflow as tf
 
 print(tf.version.VERSION)
+tf.config.list_physical_devices()
+
 IMAGENET_TEST_IMAGE = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
 UNZIP_PATH = tf.keras.utils.get_file("cats_and_dogs.zip", origin=IMAGENET_TEST_IMAGE, extract=True)
 PATH = os.path.join(os.path.dirname(UNZIP_PATH), 'cats_and_dogs_filtered')
@@ -13,6 +15,8 @@ validation_dir = os.path.join(PATH, "validation")
 
 BATCH_SIZE = 32
 IMAGE_SIZE = (160, 160)
+
+
 
 train_dataset = tf.keras.utils.image_dataset_from_directory(
     train_dir, 
@@ -29,6 +33,12 @@ validation_dataset = tf.keras.utils.image_dataset_from_directory(
 )
 
 class_names = train_dataset.class_names
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip('horizontal'),
+  tf.keras.layers.RandomRotation(0.2),
+])
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_dataset.take(1):
@@ -37,7 +47,17 @@ for images, labels in train_dataset.take(1):
         plt.imshow(images[i].numpy().astype("uint8"))
         plt.title(class_names[labels[i]])
         plt.axis("off")
-plt.show()
+#plt.show()
+
+
+val_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take(val_batches // 5)
+validation_dataset = validation_dataset.skip(val_batches // 5)
+
+print('Number of validation batches: %d' % tf.data.experimental.cardinality(validation_dataset))
+print('Number of test batches: %d' % tf.data.experimental.cardinality(test_dataset))
+
+IMAGE_SAHPE = IMAGE_SIZE + (3,)
 
 
 model = tf.keras.models.Sequential([
@@ -45,7 +65,7 @@ model = tf.keras.models.Sequential([
         input_shape=None,
         alpha=1.0,
         minimalistic=False,
-        include_top=True,
+        include_top=False,
         weights='imagenet',
         input_tensor=None,
         classes=1000,
@@ -56,14 +76,71 @@ model = tf.keras.models.Sequential([
     )
 ])
 
-# model.compile(
-#     optimizer=tf.keras.optimizers.Adam(0.001),
-#     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
-# )
+model.training = False
 
-# model.fit(
-#     ds_train,
-#     epochs=6,
-#     validation_data=ds_test,
-# )
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = model(image_batch)
+print(feature_batch.shape)
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+print(feature_batch_average.shape)
+
+prediction_layer = tf.keras.layers.Dense(1)
+prediction_batch = prediction_layer(feature_batch_average)
+print(prediction_batch.shape)
+
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+epochs = 10
+
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=['accuracy'],
+)
+
+history = model.fit(
+    train_dataset,
+    epochs=epochs,
+    validation_data=validation_dataset,
+)
+
+loss0, accuracy0 = model.evaluate(validation_dataset)
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+
+ 
+
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([min(plt.ylim()),1])
+# plt.title('Training and Validation Accuracy')
+
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.ylabel('Cross Entropy')
+# plt.ylim([0,1.0])
+# plt.title('Training and Validation Loss')
+# plt.xlabel('epoch')
+# plt.show()
---
+++ checkgpu.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy
+import os
+import tensorflow as tf
+
+print(tf.version.VERSION)
+print(tf.config.list_physical_devices())
